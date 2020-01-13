1eda5fbf04a3e0034f418d2b32c37bd04a472a31
Aruna Keshavaiah <aruna.k@position2.com>
2020-01-13 18:20:17 +0530

    updated styles

--- gulpfile.js
+++ gulpfile.js
@@ -94,6 +94,7 @@ var paths = [
   ["module-twentysix/module-scss/style.scss","module-twentysix/css","module-twentysix/module-scss/*.scss"],
   ["module-twentyseven/module-scss/style.scss","module-twentyseven/css","module-twentyseven/module-scss/*.scss"],
   ["module-twentyeight/module-scss/style.scss","module-twentyeight/css","module-twentyeight/module-scss/*.scss"],
+  ["module-twentynine/module-scss/style.scss","module-twentynine/css","module-twentynine/module-scss/*.scss"],
   ["errorpage-one/errorpage-scss/style.scss","errorpage-one/css","errorpage-one/errorpage-scss/*.scss"],
   ["errorpage-two/errorpage-scss/style.scss","errorpage-two/css","errorpage-two/errorpage-scss/*.scss"],
   ["login-one/login-scss/style.scss","login-one/css","login-one/login-scss/*.scss"],
