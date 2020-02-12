720755f2df3a61ad70cfc48bc1cb1dadf3e15f35
Balaji <balaji@Balaji-MBP-G3QJ.local>
2020-02-12 18:19:24 +0530

    updated styles and structure

--- gulpfile.js
+++ gulpfile.js
@@ -104,6 +104,7 @@ var paths = [
   ["module-thirtyfive/module-scss/style.scss","module-thirtyfive/css","module-thirtyfive/module-scss/*.scss"],
   ["module-thirtysix/module-scss/style.scss","module-thirtysix/css","module-thirtysix/module-scss/*.scss"],
   ["module-thirtyseven/module-scss/style.scss","module-thirtyseven/css","module-thirtyseven/module-scss/*.scss"],
+  ["module-thirtyeight/module-scss/style.scss","module-thirtyeight/css","module-thirtyeight/module-scss/*.scss"],
   ["errorpage-one/errorpage-scss/style.scss","errorpage-one/css","errorpage-one/errorpage-scss/*.scss"],
   ["errorpage-two/errorpage-scss/style.scss","errorpage-two/css","errorpage-two/errorpage-scss/*.scss"],
   ["login-one/login-scss/style.scss","login-one/css","login-one/login-scss/*.scss"],
